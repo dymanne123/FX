6268397e3b6fc5de18120a6576a2b235db47aa38
dymanne123 <75115606+dymanne123@users.noreply.github.com>
2022-01-05 01:48:28 +0800

    Update cluster_with_picture.py

--- FX/cluster_with_picture.py
+++ FX/cluster_with_picture.py
@@ -6,15 +6,16 @@ from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial import distance as dist
 from sklearn.cluster import KMeans
 from img_functions import img_process
+#To get a,b,c, knowing (x1,y1),(x2,y2)
 
 
 
 #cap = cv2.VideoCapture(0) # 0 means /dev/video0, 1 for /dev/video1, ...
-fig = plt.figure()
-ax1 = plt.axes(projection='3d')
+plt.ion()
+fig,axs=plt.subplots(3, 1, sharex=True)
 
 img = cv2.imread("./green_sable.png") 
-img_process(img)
+img_process(img,axs)
 
 plt.show()
 #cap.release()
