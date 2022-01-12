00c4f21a7b27fafa87a3bda0f514ab998f55a50c
dymanne123 <75115606+dymanne123@users.noreply.github.com>
2022-01-12 21:44:36 +0800

    Update img_functions.py

--- FX/img_functions.py
+++ FX/img_functions.py
@@ -1,8 +1,7 @@
 import cv2
 import numpy as np
-import heapq
 from matplotlib import pyplot as plt
-from sklearn.cluster import MiniBatchKMeans,DBSCAN
+from sklearn.cluster import MiniBatchKMeans,OPTICS
 import math
 
 #For a line: a*x+b*y+c=0, get w=(a,b) with two points(x1,y1), (x2,y2)
@@ -79,26 +78,26 @@ def display_matrix(matrix,i,j,axs,str=" ",color="red"):
     if str!=" ":
         axs[i][j].set_title(str)
     axs[i][j].set_xlim(-1,1)
+    axs[i][j].set_ylim(-1,1)
 
 #display the clusters of an array in a subplot
 def display_arr(arr,axs,str,arr_pred,i=0,j=0):
     axs[i][j].scatter(arr,np.zeros_like(arr),c=arr_pred)
     axs[i][j].set_title(str)
 
-#To ignore a small amount of green things in the environment, remove some of the largest and smallest data
-#c: the array to process; rate: percentage of data to be removed
-def pre_process_c(c,rate):
-    rate=rate/2 
-    c_num=c.shape[0]
-    point_to_delete=int(c_num*rate)
-    c=heapq.nlargest(c_num-point_to_delete,c)
-    c=heapq.nsmallest(c_num-2*point_to_delete,c)
-    return c
 
-def img_process(img,axs,pause_time=1):
+def pre_process_data(data):
+    model=OPTICS(eps=0.8, min_samples=2)
+    data_fit=model.fit(data)
+    labels=data_fit.labels_
+    data_processed=data[labels!=-1]
+    return data_processed
+
+def img_process(img,axs,pause_time=0.01,display_mode=False):
     img_gray_blurred = cv2.GaussianBlur(img, (31, 31), 0)
     img_gray_blurred[img_gray_blurred>100] = 255
     img_gray_blurred[img_gray_blurred<100] = 0
+    
     lower_green = np.array([35,43,46],dtype=np.uint8)
     upper_green = np.array([77,255,255],dtype=np.uint8)
 
@@ -128,8 +127,9 @@ def img_process(img,axs,pause_time=1):
         #knowing the start point and end point, get a2,b2 of detected lines:
         matrix_orig=np.array([get_line_ab(x1, y1, x2, y2) for x1,y1,x2,y2 in fld_segments])
         matrix_ab=np.array([get_new_w(np.array([a,b])) for a,b in matrix_orig])
-        #print(matrix_ab)
+        print(matrix_ab)
         if fld_segments.shape[0]>1:
+            matrix_ab=pre_process_data(matrix_ab)
             kmeans = MiniBatchKMeans(n_clusters=1).fit(matrix_ab)
             #print(kmeans.cluster_centers_)
             w= kmeans.cluster_centers_[0]
@@ -138,7 +138,7 @@ def img_process(img,axs,pause_time=1):
             #then get c
             distribution_c=np.array([[get_c(x1,y1,orig_w),get_c(x2,y2,orig_w)] for x1,y1,x2,y2 in fld_segments])
             distribution_c=distribution_c.reshape(-1,1)
-            distribution_c=pre_process_c(distribution_c,0.1) #0.1 means delete 10% abnormal values in the array
+            distribution_c=pre_process_data(distribution_c) 
             #print(distribution_c)
             kmeans_c=MiniBatchKMeans(n_clusters=2,random_state=0).fit(distribution_c)
             c_pred=MiniBatchKMeans(n_clusters=2).fit_predict(distribution_c)
@@ -151,14 +151,15 @@ def img_process(img,axs,pause_time=1):
         
             draw_lines(img,orig_w,c1)
             draw_lines(img,orig_w,c2) 
+            if (display_mode):
             #display the clusters and center points
-            display_matrix(matrix_orig,0,0,axs,"original a,b","red")
-            display_matrix(matrix_ab,0,1,axs,"a,b for clustering","red")
-            display_matrix(np.array([orig_w]),0,0,axs,color="blue")
-            display_matrix(np.array([w]),0,1,axs,color="blue")
-            display_arr(distribution_c,axs,"c",c_pred,1,0) 
-            axs[1][0].scatter(np.array([c1,c2]),np.zeros(2),c="red")
-            plt.show()
-            plt.pause(pause_time)         
+                display_matrix(matrix_orig,0,0,axs,"original a,b","red")
+                display_matrix(matrix_ab,0,1,axs,"a,b for clustering","red")
+                display_matrix(np.array([orig_w]),0,0,axs,color="blue")
+                display_matrix(np.array([w]),0,1,axs,color="blue")
+                display_arr(distribution_c,axs,"c",c_pred,1,0) 
+                axs[1][0].scatter(np.array([c1,c2]),np.zeros(2),c="red")
+                plt.show()
+                plt.pause(pause_time)         
     cv2.imshow("img", img)
     return   
