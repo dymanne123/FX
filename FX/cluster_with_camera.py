2d669079e4e3569a175aa7fe01b1fbddfaeecf58
Deng Yimin <yimin.deng@student-cs.fr>
2022-01-24 19:41:49 +0800

    plot wx,wy,c

--- FX/cluster_with_camera.py
+++ FX/cluster_with_camera.py
@@ -6,16 +6,25 @@ from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial import distance as dist
 from sklearn.cluster import KMeans
 from img_functions import img_process
+import time
 
 
 cap = cv2.VideoCapture(0) # 0 means /dev/video0, 1 for /dev/video1, ...
 plt.ion()
 fig,axs=plt.subplots(2,2,figsize=(12,12))
+start=time.process_time()
 
 while True :
     _, img = cap.read()
     pause_time=0.01
-    img_process(img,axs,pause_time,display_mode=False)
+    wx,wy,c=img_process(img,axs,pause_time,display_mode=False)
+    t=time.process_time()-start
+    print(wx,wy,t)
+    axs[0][0].scatter(t,wx,c="red")
+    axs[0][0].scatter(t,wy,c="blue")
+    axs[0][1].scatter(t,c,c="blue")
+    plt.show()
+    plt.pause(0.01)
     
     if cv2.waitKey(1)== ord("q"):
         break
